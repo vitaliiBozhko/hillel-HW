1f47cc185ee5eecf925b9dbae88dc9c61ce30cf7
Vitalii Bozhko <cesar.sqr999@gmail.com>
2022-07-07 15:22:28 +0200

    HW_02 change

--- HW_02/js/my.js
+++ HW_02/js/my.js
@@ -1,18 +1,18 @@
-let firstName = "Vitalii";
-let lastName = "Bozhko";
-let yearOfBirth = "1997";
+// let firstName = "Vitalii";
+// let lastName = "Bozhko";
+// let yearOfBirth = 1997;
 
+const CURRENT_YEAR = 2022;
 
-const CURRENT_YEAR = "2022";
-console.log(Number(CURRENT_YEAR));
+let yearOfBirth = prompt("Type your birth year", 1997);
+// console.log(yearOfBirth);
 
-let age = Number(CURRENT_YEAR) - Number(yearOfBirth);
+let firstName = prompt("Type your first name", 'Vitalii');
+// console.log(firstName);
 
-prompt("Type your birth year");
-console.log(age);
-prompt("Type your first name");
-console.log(firstName);
-prompt("Type your last name");
-console.log(lastName);
+let lastName = prompt("Type your last name", 'Bozhko');
+// console.log(lastName);
+
+let age = CURRENT_YEAR - yearOfBirth;
 
 console.log("User bio:", firstName, lastName ,age);
