03a1e23de30dac5c774379337330e53927050473
Vitalii Bozhko <cesar.sqr999@gmail.com>
2022-07-16 12:43:01 +0200

    HW_04 change 12.42 (16.07.22)

--- HW_04/js/my.js
+++ HW_04/js/my.js
@@ -1,23 +1,22 @@
 const myStr = prompt("Type some text", "My name is Vitalii Bozhko");
-const ID = Number(prompt(`Type some number:
+const operationId = Number(prompt(`Type some number:
 if you type 1 - you will know index
 if you type 2 - you delete selected letter
 if you type 3 - you change selected letter
 if you type 4 - you will find out how many letters are in a string`));
 
-const arr =["=)", "=(", ":)", ":(", "=D", ":D", "=P", ":P", ";D", ";)"];
-let smileIndex = getRandomIntInclusive(0, 9);
-
-if (ID === 1) {
+if (operationId === 1) {
     let chooseNumber = Number(prompt("which letter index do you want to know (start from 0):", "4"));
     console.log(myStr.charCodeAt(chooseNumber));
-} else if (ID === 2) {
+} else if (operationId === 2) {
     let chooseNumber = Number(prompt("Which letter will you delete:", "6"));
-    console.log(myStr.replace(`${myStr[chooseNumber]}`, ""));
-} else if (ID === 3) {
+    console.log(myStr.slice(0, chooseNumber) + myStr.slice(chooseNumber + 1));
+} else if (operationId === 3) {
+    const arr =["=)", "=(", ":)", ":(", "=D", ":D", "=P", ":P", ";D", ";)"];
     let chooseNumber = Number(prompt("Which letter will you change:", "3"));
-    console.log(myStr.replace(`${myStr[chooseNumber]}`, `${arr[smileIndex]}`));
-} else if (ID === 4) {
+    let smileIndex = getRandomIntInclusive(0, 9);
+    console.log(myStr.slice(0, chooseNumber) + arr[smileIndex] + myStr.slice(chooseNumber + 1));
+} else if (operationId === 4) {
     console.log(myStr.replaceAll(" ", "").length);
 }
 
