7b1c727004cd7b571a8eb6b4a534928ccda81890
Vitalii Bozhko <cesar.sqr999@gmail.com>
2022-07-24 12:23:07 +0200

    HW_06 change 12.22 (24.07.22)

--- HW_06/js/my.js
+++ HW_06/js/my.js
@@ -2,16 +2,23 @@ let bun;
 let hamburger = `hamburger`;
 let cheeseburger = `cheeseburger`;
 let price = 0;
+const two = 2;
+const three = 3;
+const five = 5;
+const ten = 10;
+const fifteen = 15;
+const twenty = 20;
+
 do {
     bun = prompt(`Choose select bun: hamburger or cheeseburger`, `hamburger`);
     if (bun === hamburger) {
-        price += 10;
+        price += ten;
     } else if (bun === cheeseburger) {
-        price += 15;
+        price += fifteen;
 
         let doubleCheese = false;
         doubleCheese = confirm(`Would you like to add double cheese?`);
-        if (doubleCheese) price += 5;
+        if (doubleCheese) price += five;
     }
 } while (bun === null || !bun || (bun !== hamburger && bun !== cheeseburger));
 
@@ -23,8 +30,8 @@ if (potato) {
     let potatoLarge = `big`;
     potatoSize = prompt(`Choose potato size: small/middle/big`, potatoSmall);
     if (potatoSize === potatoSmall || potatoSize === null || !potatoSize) price += 10;
-    else if (potatoSize === potatoMedium) price += 15;
-    else if (potatoSize === potatoLarge) price += 20;
+    else if (potatoSize === potatoMedium) price += fifteen;
+    else if (potatoSize === potatoLarge) price += twenty;
 }
 
 let sauce = confirm(`Would you like sauce?`);
@@ -34,25 +41,15 @@ if (sauce) {
     let mayonnaise = `mayonnaise`;
     typeOfSauce = prompt(`Choose sauce: ketchup/mayonnaise`, ketchup);
 
-    if (typeOfSauce === ketchup || typeOfSauce === null || !typeOfSauce) price += 2
-    else if (typeOfSauce === mayonnaise) price += 3;
+    if (typeOfSauce === ketchup || typeOfSauce === null || !typeOfSauce) price += two
+    else if (typeOfSauce === mayonnaise) price += three;
 }
 
-let order = bun && potatoSize && typeOfSauce;
-if (order) {
 document.write(`<h2>Your order is:</h2>
-                <ol>
-                    <li>Bun is: ${bun};</li>
-                    <li>Potato size is: ${potatoSize};</li>
-                    <li>Sauce is: ${typeOfSauce};</li>
-                </ol>
-                <p>Price is: ${price}.</p>
-`)
-} else if (!order) {
-    document.write(`<h2>Your order is:</h2>
-                <ol>
-                    <li>Bun is: ${bun};</li>
-                </ol>
-                <p>Price is: ${price}.</p>
-`)
-}
+    <ol>
+        <li>Bun is: ${bun};</li>
+        ${potato ? `<li>Potato size is: ${potatoSize};</li>` : ``}
+        ${sauce ? `<li>Sauce is: ${typeOfSauce};</li>` : ``}
+    </ol>
+    <p>Price is: ${price}.</p>
+`);
