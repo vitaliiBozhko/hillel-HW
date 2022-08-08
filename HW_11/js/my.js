cdad3d62f0417a57de262e596196db9567c6e8c7
Vitalii Bozhko <cesar.sqr999@gmail.com>
2022-08-08 19:11:54 +0200

    HW_11 change 19.11 (08.08.22)

--- HW_11/js/my.js
+++ HW_11/js/my.js
@@ -5,32 +5,23 @@ const products = [
     ['grape',18]
 ];
 
-function getPrice(products, seasonFunc) {
+let getPrice = (products, seasonFunc) => {
     let copiedProducts = JSON.parse(JSON.stringify(products));
     let sumResult = 0;
     for (let i=0; i<copiedProducts.length; i++) {
         for (let j=0;j<copiedProducts[i].length;j++) {
             if (typeof copiedProducts[i][j] === `number`) {
-                if (seasonFunc === summerValue) {
-                    sumResult += summerValue(copiedProducts[i][j])
-                } else if (seasonFunc === winterValue) {
-                    sumResult += winterValue(copiedProducts[i][j])
-                } else if (seasonFunc !== summerValue && seasonFunc !== winterValue) {
-                    sumResult += copiedProducts[i][j]
-                }
+                if (seasonFunc === summerValue) sumResult += summerValue(copiedProducts[i][j]);
+                else if (seasonFunc === winterValue) sumResult += winterValue(copiedProducts[i][j]);
+                else if (seasonFunc !== summerValue && seasonFunc !== winterValue) sumResult += copiedProducts[i][j];
             }
         }
     }
     return sumResult;
 }
 
-function summerValue(value){
-    return value*0.8;
-}
-
-function winterValue(value){
-    return value*2;
-}
+let  summerValue = value => value*0.8;
+let winterValue = value => value*2;
 
 console.log( getPrice(products) );
 console.log( getPrice(products, summerValue) );
