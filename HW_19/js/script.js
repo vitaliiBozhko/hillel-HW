432447056699ee837262a92a9d3def418e147f00
Vitalii Bozhko <cesar.sqr999@gmail.com>
2022-09-13 18:03:39 +0200

    HW_19 change 18.00 (13.09.22)

--- HW_19/js/script.js
+++ HW_19/js/script.js
@@ -5,25 +5,41 @@ block.style.left = `0`;
 block.style.top = `0`;
 
 const STEP = 10;
-
+const showBems = () => {
+    block.innerHTML = `BEMS`;
+    block.style.color = `white`;
+    setTimeout(() => block.innerHTML = ``, 2000);
+}
 const moveLeft = (block) => {
-    console.log(`Відступ блоку від ЛІВОГО краю body:`, block.offsetLeft);
-
     if((block.offsetLeft) < 0){
+        showBems();
         block.style.left = parseInt(block.style.left) + STEP*2 + `px`;
     } else{
         block.style.left = parseInt(block.style.left) - STEP + `px`;
     }
 };
-
 const moveRight = (block) => {
-    console.log(`Відступ блоку від ЛІВОГО краю body:`, block.offsetLeft);
-    console.log(`block.offsetLeft + block.clientWidth = Відступ блоку від ПРАВОГО краю body`, block.offsetLeft + block.clientWidth);
-
     if((block.offsetLeft + block.clientWidth) > body.clientWidth){
+        showBems();
         block.style.left = parseInt(block.style.left) - STEP*2 + `px`;
     } else{
         block.style.left = parseInt(block.style.left) + STEP + `px`;
+    }
+};
+const moveUp = (block) => {
+    if((block.offsetTop) < 0){
+        showBems();
+        block.style.top = parseInt(block.style.top) + STEP*2 + `px`;
+    } else{
+        block.style.top = parseInt(block.style.top) - STEP + `px`;
+    }
+};
+const moveDown = (block) => {
+    if((block.offsetTop + block.clientHeight) > body.clientHeight){
+        showBems();
+        block.style.top = parseInt(block.style.top) - STEP*2 + `px`;
+    } else{
+        block.style.top = parseInt(block.style.top) + STEP + `px`;
     }
 };
 const jumpUp = (block) => {
@@ -50,7 +66,9 @@ const EVENTS = {
     39: block => moveRight(block),
     37: block => moveLeft(block),
     32: block => jumpUp(block),
-    17: block => sitDown(block)
+    17: block => sitDown(block),
+    38: block => moveUp(block),
+    40: block => moveDown(block)
 }
 
 document.addEventListener(`keydown`, event => {
