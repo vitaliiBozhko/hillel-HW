705463471b3bb1e07e838863788fb2a19670c922
Vitalii Bozhko <cesar.sqr999@gmail.com>
2022-09-28 15:33:31 +0200

    HW_22 change 15.30 (28.09.22)

--- HW_22/js/script.js
+++ HW_22/js/script.js
@@ -1,23 +1,40 @@
 const jokesCategories = document.querySelector(`#jokesCategories`);
-const jokesList = document.querySelector(`#jokesList`);
-
-const API = `https://api.chucknorris.io/jokes/categories`;
+const jokesContainer = document.querySelector(`#ulContainer`);
+const API = `https://api.chucknorris.io/jokes`;
 
 const getData = url => fetch(url)
     .then(data => data.ok ? data.json() : Promise.catch(data.statusText))
     .catch(err => console.log(`In catch: ${err}`));
 
 const renderSelectOptions = () => {
-    getData(API)
+    getData(API+`/categories`)
         .then(categories => {
             jokesCategories.innerHTML = categories
-                .map(category => `<option>${category}</option>`)
+                .map((category) => `<option value="${category}">${category}</option>`)
         })
-
 }
+renderSelectOptions();
 
-const renderLIs = () => {
-    console.log(getData(API + `=${categories}`))
-}
-renderSelectOptions()
-renderLIs()
+jokesCategories.addEventListener(`change`, () => {
+    let selectedOption = jokesCategories.querySelector(`option[value="${jokesCategories.value}"]`);
+    selectedOption.setAttribute(`disabled`, `false`);
+    getData(API+`/random?category=${selectedOption.value}`)
+        .then(object => {
+
+            const jokesList = document.createElement(`ul`);
+            jokesList.className = `jokesList`;
+            jokesList.innerHTML = `<li>
+                <p>Category: <b>${selectedOption.value}</b></p>
+                <p>${object.value}</p> 
+            </li>`
+
+            const deleteBtn = document.createElement(`button`);
+            deleteBtn.innerHTML = `Remove joke`;
+            deleteBtn.addEventListener(`click`, () => {
+                jokesList.remove();
+            })
+
+            jokesList.append(deleteBtn);
+            jokesContainer.append(jokesList);
+        })
+});
